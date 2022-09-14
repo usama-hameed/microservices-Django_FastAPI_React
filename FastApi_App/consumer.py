78fafe75d38dece4def42dfb12dcfa601eaaad1e
usama-hameed <musamahameed97@gmail.com>
2022-09-14 19:40:37 +0500

    data consistency between both apps

--- FastApi_App/consumer.py
+++ FastApi_App/consumer.py
@@ -1,4 +1,8 @@
 import pika
+from .models import Product
+from fastapi_sqlalchemy import db
+
+import json
 
 # amqps://bescccok:rsjlBxeksDvwcp23H0QDJh83qttksbsF@sparrow.rmq.cloudamqp.com/bescccok
 
@@ -11,7 +15,22 @@ channel = connection.channel()
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body.decode('utf-8'))
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
--- FastApi_App/main.py
+++ FastApi_App/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
+from fastapi.responses import Response
 
 from schema import ProductSchema, ProductUserSchema
 from models import Product, ProductUser
@@ -12,10 +13,12 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url='postgresql://postgres:admin123@localhost/main')
 
 
-@app.get('/')
+@app.get('/api/products')
 def index():
-    return "Usama"
+    products = Product.query.all()
+    return Response(products, status_code=200)
 
+@app
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8001)
